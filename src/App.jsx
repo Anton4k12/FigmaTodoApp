2fe214e5fcb906560b8c1ac4007a5d6aa542c098
Anton4k12 <neanton2008@gmail.com>
2024-10-16 22:20:08 +0300

    feat: edit todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import PlusIcon from "@/assets/plus.svg?react";
 import DeleteIcon from "@/assets/delete.svg?react";
 import EditIcon from "@/assets/edit.svg?react";
 import ConfirmIcon from "@/assets/confirm.svg?react";
+import CancelIcon from "@/assets/cancel.svg?react";
 
 import { useState } from "react";
 
 export const App = () => {
   const [tasks, setTasks] = useState([]);
   const [inputValue, setInputValue] = useState("");
+  const [editingTaskId, setEditingTaskId] = useState();
 
   const handleAddTask = () => {
     setTasks([
@@ -25,15 +27,40 @@ export const App = () => {
     );
   };
 
-  const handleTaskDone = () => {
-    setTasks([{ done: true }]);
+  const handleTaskDone = (confirmedTaskId) => {
+    const nextTasks = tasks.map((task) => {
+      if (task.id === confirmedTaskId) {
+        return { ...task, done: true };
+      } else return task;
+    });
+    setTasks(nextTasks);
+  };
+
+  const handleEditTask = (idToEdit) => {
+    setEditingTaskId(idToEdit);
+  };
+
+  const handleEditConfirm = (newName) => {
+    const nextEditedTasks = tasks.map((task) => {
+      if (task.id === editingTaskId) {
+        return { ...task, name: newName };
+      } else return task;
+    });
+    setTasks(nextEditedTasks);
+    setEditingTaskId(undefined);
+  };
+
+  const handleEditCancel = () => {
+    setEditingTaskId(undefined);
   };
 
   const doneTasks = tasks.filter((task) => {
     return task.done;
   });
 
-  console.log({ doneTasks });
+  const undoneTasks = tasks.filter((task) => {
+    return !task.done;
+  });
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-main-bg">
@@ -57,38 +84,102 @@ export const App = () => {
 
         <div className="flex flex-col gap-4">
           <div>
-            {tasks.length === 0
-              ? "You don't have any tasks yet"
-              : `Tasks to do - ${tasks.length}`}
+            {undoneTasks.length === 0 ? (
+              <p className="text-center text-white/65">
+                You don't have tasks to do yet
+              </p>
+            ) : (
+              `Tasks to do - ${undoneTasks.length}`
+            )}
           </div>
 
-          {tasks.map((task) => {
-            return (
-              <>
-                <div
-                  key={task.id}
-                  className="flex justify-between rounded-[10px] bg-card p-5 text-main-foreground"
-                >
-                  {task.name}
-                  <div className="flex gap-2">
-                    <button onClick={() => handleDeleteTask(task.id)}>
-                      <ConfirmIcon className="size-5"></ConfirmIcon>
-                    </button>
-
-                    <button>
-                      <EditIcon className="size-5"></EditIcon>
-                    </button>
-
-                    <button onClick={() => handleTaskDone()}>
-                      <DeleteIcon className="size-5"></DeleteIcon>
-                    </button>
-                  </div>
-                </div>
-              </>
+          {undoneTasks.map((task) => {
+            return task.id !== editingTaskId ? (
+              <Task
+                task={task}
+                onTaskDelete={handleDeleteTask}
+                onTaskDone={handleTaskDone}
+                onTaskEdit={handleEditTask}
+              ></Task>
+            ) : (
+              <EditingTask
+                task={task}
+                onEditConfirm={handleEditConfirm}
+                onEditCancel={handleEditCancel}
+              ></EditingTask>
             );
           })}
         </div>
+
+        {doneTasks.length > 0 && (
+          <div className="flex flex-col gap-4">
+            <div>Done - {doneTasks.length}</div>
+
+            {doneTasks.map((task) => {
+              return (
+                <>
+                  <div
+                    key={task.id}
+                    className="flex justify-between rounded-[10px] bg-card p-5 text-[#78CFB0] line-through"
+                  >
+                    {task.name}
+                  </div>
+                </>
+              );
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
 };
+
+function Task({ task, onTaskDone, onTaskDelete, onTaskEdit }) {
+  return (
+    <div
+      key={task.id}
+      className="flex justify-between rounded-[10px] bg-card p-5 text-main-foreground"
+    >
+      {task.name}
+      <div className="flex gap-2">
+        <button onClick={() => onTaskDone(task.id)}>
+          <ConfirmIcon className="size-5"></ConfirmIcon>
+        </button>
+
+        <button onClick={() => onTaskEdit(task.id)}>
+          <EditIcon className="size-5"></EditIcon>
+        </button>
+
+        <button onClick={() => onTaskDelete(task.id)}>
+          <DeleteIcon className="size-5"></DeleteIcon>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+function EditingTask({ task, onEditConfirm, onEditCancel }) {
+  const [editingInputText, setEditingInputText] = useState(task.name);
+  return (
+    <div
+      key={task.id}
+      className="flex justify-between rounded-[10px] bg-card p-5 text-main-foreground"
+    >
+      <input
+        type="text"
+        value={editingInputText}
+        onChange={(e) => setEditingInputText(e.target.value)}
+        className="-translate-x-1 rounded-[10px] border border-main-foreground bg-transparent px-1 py-0.5 ring-main-foreground focus:outline-none focus:ring"
+      />
+      <div className="flex gap-2">
+        <button onClick={() => onEditConfirm(editingInputText)}>
+          <ConfirmIcon className="size-5"></ConfirmIcon>
+        </button>
+
+        <button onClick={() => onEditCancel(task.id)}>
+          <CancelIcon className="size-5"></CancelIcon>
+        </button>
+      </div>
+    </div>
+  );
+}
