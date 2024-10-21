e9bfea41619c5e7cb215915a7b9071a3adea0606
Anton4k12 <neanton2008@gmail.com>
2024-10-21 21:25:11 +0300

    feat: git link

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,12 @@ import CancelIcon from "@/assets/cancel.svg?react";
 import { useState } from "react";
 import { useLocalStorage } from "usehooks-ts";
 
+const initialId = crypto.randomUUID();
+
 export const App = () => {
-  const [tasks, setTasks] = useLocalStorage("tasks", []);
+  const [tasks, setTasks] = useLocalStorage("tasks", [
+    { name: "learn react", id: initialId, done: false },
+  ]);
   const [inputValue, setInputValue] = useState("");
   const [editingTaskId, setEditingTaskId] = useState();
 
@@ -64,7 +68,7 @@ export const App = () => {
   });
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-main-bg">
+    <div className="relative flex min-h-screen items-center justify-center bg-main-bg">
       <div className="flex min-w-[580px] flex-col justify-center gap-14 rounded-3xl bg-secondary-bg px-12 py-12">
         <div className="flex h-10 w-full items-center gap-2.5">
           <input
@@ -130,6 +134,32 @@ export const App = () => {
             })}
           </div>
         )}
+      </div>
+
+      <div className="absolute bottom-4 left-1/2 flex w-fit -translate-x-1/2 items-center bg-black text-xs text-white/40">
+        <div className="flex items-center gap-2">
+          <div className="flex gap-1">
+            <div> made by </div>
+            <a target="_blank" href="https://github.com/Anton4k12">
+              Anton4k12
+            </a>
+          </div>
+          <a target="_blank" href="https://github.com/Anton4k12">
+            <svg
+              height={20}
+              aria-hidden="true"
+              viewBox="0 0 24 24"
+              width={20}
+              data-view-component="true"
+              className="octicon octicon-mark-github v-align-middle color-fg-default"
+            >
+              <path
+                d="M12.5.75C6.146.75 1 5.896 1 12.25c0 5.089 3.292 9.387 7.863 10.91.575.101.79-.244.79-.546 0-.273-.014-1.178-.014-2.142-2.889.532-3.636-.704-3.866-1.35-.13-.331-.69-1.352-1.18-1.625-.402-.216-.977-.748-.014-.762.906-.014 1.553.834 1.769 1.179 1.035 1.74 2.688 1.25 3.349.948.1-.747.402-1.25.733-1.538-2.559-.287-5.232-1.279-5.232-5.678 0-1.25.445-2.285 1.178-3.09-.115-.288-.517-1.467.115-3.048 0 0 .963-.302 3.163 1.179.92-.259 1.897-.388 2.875-.388.977 0 1.955.13 2.875.388 2.2-1.495 3.162-1.179 3.162-1.179.633 1.581.23 2.76.115 3.048.733.805 1.179 1.825 1.179 3.09 0 4.413-2.688 5.39-5.247 5.678.417.36.776 1.05.776 2.128 0 1.538-.014 2.774-.014 3.162 0 .302.216.662.79.547C20.709 21.637 24 17.324 24 12.25 24 5.896 18.854.75 12.5.75Z"
+                className="fill-white/40"
+              />
+            </svg>
+          </a>
+        </div>
       </div>
     </div>
   );
